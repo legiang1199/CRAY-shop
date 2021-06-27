b57ec1f81867cc72583d63cd9b91dc4c898fff8f
Giang Nam <legiang1199@gmail.com>
2021-06-27 20:59:07 +0700

    Update code database

--- database.php
+++ database.php
@@ -3,11 +3,12 @@ $servername = "ec2-35-169-188-58.compute-1.amazonaws.com";
 $usernameDB = "ecwkpszubktrca";
 $password = "29596da29b68a0c25430b95c8324d819eea14be7cda2748cc88b841b4609098a";
 $db = "d89f584cahc1bq";
+$uri = 'postgres://ecwkpszubktrca:29596da29b68a0c25430b95c8324d819eea14be7cda2748cc88b841b4609098a@ec2-35-169-188-58.compute-1.amazonaws.com:5432/d89f584cahc1bq';
 // Create connection
-$conn = pg_connect($servername, $usernameDB, $password,$db);
+$conn = pg_connect($uri);
 
 // Check connection
 if (!$conn) {
-  die("Connection failed: " . pg_connect_error());
+  die('Error: Could not connect: ' . pg_last_error());
 }
 ?>
