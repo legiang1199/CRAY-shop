d24426cb4a7a2ecaabd4b0f7403b55464c814c24
Giang Nam <legiang1199@gmail.com>
2021-07-05 21:45:48 +0700

    Upadate index

--- index.php
+++ index.php
@@ -28,18 +28,6 @@
                     </div>
                 </div>
             </form>
-            <!-- Navbar-->
-            <ul class="navbar-nav ml-auto ml-md-0">
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" id="userDropdown" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
-                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
-                        <a class="dropdown-item" href="#">login</a>
-                        <a class="dropdown-item" href="#">Logup</a>
-                        <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="login.html">Logout</a>
-                    </div>
-                </li>
-            </ul>
         </nav>
         <div id="layoutSidenav">
             <div id="layoutSidenav_nav">
